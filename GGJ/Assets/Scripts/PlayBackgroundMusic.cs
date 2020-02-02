54bf89d72b050d683cd7b7e29ba0c9bc34a69f99
Xueer Zhu <sherryxueerzhu@gmail.com>
2020-02-01 20:17:55 -0800

    add sounds and music, with player

---
+++ GGJ/Assets/Scripts/PlayBackgroundMusic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayBackgroundMusic : MonoBehaviour
+{
+    public AudioClip musicToPlay;
+    private AudioSource musicAudioSource;
+
+
+    public static PlayBackgroundMusic Music = null;
+
+
+    void Awake()
+    {
+        if (Music == null) {
+            Music = this;
+        }
+        
+
+        DontDestroyOnLoad(gameObject);
+
+        musicAudioSource = GetComponent<AudioSource>();
+        musicAudioSource.Play();
+    }
+}
